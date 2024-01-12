c97b30b54111a34648c048b6baa034103e8e7483
Muhammad Usman <m.usman@solvefy.io>
2024-01-12 02:15:20 -0500

    Animation apply with pan responder

--- screens/Notification.js
+++ screens/Notification.js
@@ -1,10 +1,29 @@
-import { View, Text } from 'react-native'
+import { View, Text, Animated, StatusBar } from 'react-native'
 import React from 'react'
 
 const Notification = () => {
+  const position = new Animated.ValueXY({x:0,y:0})
+  Animated.timing(position, {
+    toValue: {x: 200, y: 500}
+  }).stop()
   return (
-    <View>
+    <View style={{}}>
       <Text>Notification</Text>
+
+      <Animated.View style={{
+        marginTop: 40,
+        height: 80,
+        width: 80,
+         justifyContent: 'center',
+         alignItems: 'center',
+         backgroundColor: 'red',
+         transform: [{
+          translateX: position.x
+         }]
+      }}>
+
+      <Text>Notification</Text>
+      </Animated.View>
     </View>
   )
 }
--- screens/Options.js
+++ screens/Options.js
@@ -1,10 +1,66 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Animated, PanResponder } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
 
 const Options = () => {
+  const [animationComplet, setAnimationComplete] = useState(false)
+
+  const position = new Animated.ValueXY({ x: 0, y: 0 })
+  const opacity = useRef(new Animated.Value(0)).current;  
+  // useEffect(() => {
+    // Animated.timing(position, {
+    //   toValue: { x: 200, y: 500 },
+    //   duration: 2000
+    // }).start(() => {
+    //   position.setValue({ x: 0, y: 0 });
+    // })
+
+  // }, [])
+  const routate = position.x.interpolate({
+    inputRange: [0, 100],
+    outputRange: ['0deg', '360deg']
+  })
+  const pan = PanResponder.create({
+    onMoveShouldSetPanResponder: () => true,
+    onPanResponderMove: (e, gesture) => {
+      position.setValue({x: gesture.dx, y: gesture.dy})
+    },
+    // onPanResponderMove: Animated.event([
+    //   null,
+    //   {dx: position.x, dy: position.y}
+    // ]),
+    onPanResponderRelease: () => {
+      position.setValue({x: 0, y: 0})
+    }
+  })
+  // useEffect(() => {
+  //   // Start the animation when the component mounts
+  //   Animated.timing(opacity, {
+  //     toValue: 1,
+  //     duration: 1000,
+  //     useNativeDriver: true,
+  //   }).start();
+  // }, []);
   return (
-    <View>
-      <Text>Options</Text>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      {/* <Text>Option</Text> */}
+
+      <Animated.View {...pan.panHandlers} style={[{
+        marginTop: 40,
+        height: 80,
+        width: 80,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'red',
+        transform: [
+        {translateX: position.x},
+        {translateY: position.y},
+        {rotate: routate}
+        ]
+      }
+        ]}>
+
+        <Text>Notification</Text>
+      </Animated.View>
     </View>
   )
 }
